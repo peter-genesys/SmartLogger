6769d71f8c5412f95dabda7e7fca45ff96192617
Peter Burgess <peter.a.burgess@gmail.com>
2014-06-05 10:11:35 +1000

    NEW Patch: LOG-01_08A_08B_LOGGER - Fixed orphaned error nodes

---
+++ patch/feature/logger/LOG-01/LOG-01_08A_08B_LOGGER/install.sql
@@ -0,0 +1,34 @@
+PROMPT LOG TO LOG-01_08A_08B_LOGGER.log
+PROMPT .
+SET AUTOCOMMIT OFF
+SET AUTOPRINT ON
+SET ECHO ON
+SET FEEDBACK ON
+SET PAUSE OFF
+SET SERVEROUTPUT ON SIZE 1000000
+SET TERMOUT ON
+SET TRIMOUT ON
+SET VERIFY ON
+SET trims on pagesize 3000
+SET auto off
+SET verify off echo off define on
+WHENEVER OSERROR EXIT FAILURE ROLLBACK
+WHENEVER SQLERROR EXIT FAILURE ROLLBACK
+
+SPOOL LOG-01_08A_08B_LOGGER.log
+CONNECT LOGGER/&&LOGGER_password@&&database
+set serveroutput on;
+select user||'@'||global_name Connection from global_name;
+
+
+PROMPT PACKAGE BODIES
+
+PROMPT ms_logger.pkb 
+@feature\logger\LOG-01\LOG-01_08A_08B_LOGGER\ms_logger.pkb;
+Show error;
+
+COMMIT;
+COMMIT;
+PROMPT 
+PROMPT install.sql - COMPLETED.
+spool off;
