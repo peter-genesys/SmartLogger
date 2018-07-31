5d78d9ff3a63178e07312de085a750cb320edaf3
Peter Burgess <peter.a.burgess@gmail.com>
2018-07-31 13:00:43 +1000

    NEW Patch: Rebranding_02_ENDUSER - Rebranding for synonyms

---
+++ patch/feature/Rebranding/Rebranding_02_ENDUSER/install_lite.sql
@@ -0,0 +1,40 @@
+PROMPT LOG TO Rebranding_02_ENDUSER.log
+PROMPT .
+SET AUTOCOMMIT OFF
+SET AUTOPRINT ON
+SET ECHO ON
+SET FEEDBACK ON
+SET PAUSE OFF
+SET SERVEROUTPUT ON SIZE 1000000
+SET TERMOUT ON
+SET TRIMOUT ON
+SET VERIFY ON
+SET trims on pagesize 3000
+SET auto off
+SET verify off echo off define on
+WHENEVER OSERROR EXIT FAILURE ROLLBACK
+WHENEVER SQLERROR EXIT FAILURE ROLLBACK
+
+define patch_name = 'Rebranding_02_ENDUSER'
+define patch_desc = 'Rebranding for synonyms'
+define patch_path = 'feature/Rebranding/Rebranding_02_ENDUSER/'
+SPOOL Rebranding_02_ENDUSER.log
+CONNECT &&ENDUSER_user/&&ENDUSER_password@&&database
+set serveroutput on;
+select user||'@'||global_name Connection from global_name;
+
+
+PROMPT SYNONYMS
+
+PROMPT logger.syn 
+@&&patch_path.logger.syn;
+
+COMMIT;
+COMMIT;
+PROMPT 
+PROMPT install_lite.sql - COMPLETED.
+spool off;
+
+
+COMMIT;
+
