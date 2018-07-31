c3acaf6f5b3f7d551c4d5cf848df89a4acdeb453
Peter Burgess <peter.a.burgess@gmail.com>
2018-07-31 16:49:21 +1000

    New Config
    Modified   database/LOGGER/Data/sm_config_pop.sql
    --LOGGER_WORKSPACE, PROMO_LEVEL (override)

--- database/LOGGER/Data/sm_config_pop.sql
+++ database/LOGGER/Data/sm_config_pop.sql
@@ -13,10 +13,12 @@ Insert into sm_config (NAME,VALUE) values ('APEX_PORT'      ,'8080');
 Insert into sm_config (NAME,VALUE) values ('APEX_DIR'       ,'ords');
 Insert into sm_config (NAME,VALUE) values ('KEEP_DAY_COUNT' ,'1');
 
-Insert into sm_config (NAME,VALUE) values ('LOGGER_WORKSPACE' ,'LOGGER');
 Insert into sm_config (NAME,VALUE) values ('SMTP_MAIL' ,'N');
 Insert into sm_config (NAME,VALUE) values ('APEX_MAIL' ,'Y');
 
+Insert into sm_config (NAME,VALUE) values ('LOGGER_WORKSPACE' ,'LOGGER');
+Insert into sm_config (NAME,VALUE) values ('SMARTLOGGER_APP_ALIAS' ,'SMARTLOGGER_DEV');
+Insert into sm_config (NAME,VALUE) values ('PROMO_LEVEL' ,''); 
 
 Insert into sm_config (NAME,VALUE) values ('DEFAULT_AUTO_DEBUG'    ,'Y');
 Insert into sm_config (NAME,VALUE) values ('DEFAULT_AUTO_NORMAL'   ,'N');
