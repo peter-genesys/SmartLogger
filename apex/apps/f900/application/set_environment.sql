bcb7a3485c0ba9f026d54fa14f7a33e082248fea
Peter Burgess <peter.a.burgess@gmail.com>
2018-09-24 17:39:30 +1000

    Apex2GIT App 900 - SmartLogger - VM_12C
    QHIDS-1915 - Apex Cloned Session support
    Show child sessions in the tree as a child node of the parent session.
    
    Added      apex/apps/f900/application/set_environment.sql
    Modified   apex/apps/f900/application/create_application.sql
    Modified   apex/apps/f900/application/init.sql
    Modified   apex/apps/f900/application/pages/page_00030.sql
    --NEW column PARENT_APP_SESSION
    Modified   apex/apps/f900/application/pages/page_00040.sql
    --Both reports use single view sm_call_tree_v
    --NEW item P40_APP_USER
    --NEW icon for Child Sessions fa-user-circle-o
    Modified   apex/apps/f900/install.sql

---
+++ apex/apps/f900/application/set_environment.sql
@@ -0,0 +1,50 @@
+prompt --application/set_environment
+ 
+prompt APPLICATION 900 - SmartLogger
+--
+-- Application Export:
+--   Application:     900
+--   Name:            SmartLogger
+--   Exported By:     LOGGER
+--   Flashback:       0
+--   Export Type:     Application Export
+--   Version:         5.1.0.00.45
+--   Instance ID:     230165337719552
+--
+
+-- Application Statistics:
+--   Pages:                     19
+--     Items:                   44
+--     Validations:              1
+--     Processes:               39
+--     Regions:                 53
+--     Buttons:                 59
+--     Dynamic Actions:         24
+--   Shared Components:
+--     Logic:
+--       Items:                  5
+--     Navigation:
+--       Lists:                  2
+--       Breadcrumbs:            1
+--         Entries:             16
+--       NavBar Entries:         1
+--     Security:
+--       Authentication:         3
+--     User Interface:
+--       Themes:                 1
+--       Templates:
+--         Page:                 9
+--         Region:              15
+--         Label:                5
+--         List:                11
+--         Popup LOV:            1
+--         Calendar:             1
+--         Breadcrumb:           1
+--         Button:               3
+--         Report:               9
+--       Shortcuts:              5
+--       Plug-ins:               5
+--     Globalization:
+--     Reports:
+--   Supporting Objects:  Excluded
+
