70d7a977e4a5fe43bf670239db9d7ed40b12f0bb
peter.a.burgess <peter.a.burgess@gmail.com>
2018-10-03 12:39:14 +1000

    Apex2GIT App 900 - SmartLogger - DCAS_DEV
    Fixes for Apex Context
    
    Modified   apex/apps/f900/application/init.sql
    Modified   apex/apps/f900/application/pages/page_00035.sql
    --FIX links and display conditions
    Missing    apex/apps/f900/application/set_environment.sql
    Modified   apex/apps/f900/install.sql

--- apex/apps/f900/application/init.sql
+++ apex/apps/f900/application/init.sql
@@ -13,10 +13,59 @@ whenever sqlerror exit sql.sqlcode rollback
 begin
 wwv_flow_api.import_begin (
  p_version_yyyy_mm_dd=>'2016.08.24'
-,p_release=>'5.1.0.00.45'
+,p_release=>'5.1.4.00.08'
 ,p_default_workspace_id=>12287443100895424
 ,p_default_application_id=>900
 ,p_default_owner=>'LOGGER'
 );
 end;
 /
+ 
+prompt APPLICATION 900 - SmartLogger
+--
+-- Application Export:
+--   Application:     900
+--   Name:            SmartLogger
+--   Exported By:     LOGGEROWN
+--   Flashback:       0
+--   Export Type:     Application Export
+--   Version:         5.1.4.00.08
+--   Instance ID:     63712309566204
+--
+
+-- Application Statistics:
+--   Pages:                     22
+--     Items:                   52
+--     Validations:              1
+--     Processes:               44
+--     Regions:                 60
+--     Buttons:                 63
+--     Dynamic Actions:         27
+--   Shared Components:
+--     Logic:
+--       Items:                  8
+--     Navigation:
+--       Lists:                  2
+--       Breadcrumbs:            1
+--         Entries:             17
+--       NavBar Entries:         2
+--     Security:
+--       Authentication:         3
+--     User Interface:
+--       Themes:                 1
+--       Templates:
+--         Page:                 9
+--         Region:              15
+--         Label:                5
+--         List:                11
+--         Popup LOV:            1
+--         Calendar:             1
+--         Breadcrumb:           1
+--         Button:               3
+--         Report:               9
+--       Shortcuts:              5
+--       Plug-ins:               6
+--     Globalization:
+--     Reports:
+--   Supporting Objects:  Excluded
+
