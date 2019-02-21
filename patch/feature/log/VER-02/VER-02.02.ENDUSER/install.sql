f006758180e4d596651890568ef1e414d5905dba
Peter Burgess <peter.a.burgess@gmail.com>
2019-02-21 22:43:49 +1000

    NEW Patch: VER-02.02.ENDUSER - Private synonyms for an end user

---
+++ patch/feature/log/VER-02/VER-02.02.ENDUSER/install.sql
@@ -0,0 +1,72 @@
+PROMPT LOG TO VER-02.02.ENDUSER.log
+PROMPT .
+SET AUTOCOMMIT OFF
+SET AUTOPRINT ON
+SET ECHO ON
+SET FEEDBACK ON
+SET PAUSE OFF
+SET SERVEROUTPUT ON SIZE 1000000
+SET TERMOUT ON
+SET TRIMOUT ON
+SET VERIFY ON
+SET trims on pagesize 3000
+SET auto off
+SET verify off echo off define on
+WHENEVER OSERROR EXIT FAILURE ROLLBACK
+WHENEVER SQLERROR EXIT FAILURE ROLLBACK
+
+define patch_name = 'VER-02.02.ENDUSER'
+define patch_desc = 'Private synonyms for an end user'
+define patch_path = 'feature/log/VER-02/VER-02.02.ENDUSER/'
+SPOOL VER-02.02.ENDUSER.log
+CONNECT &&ENDUSER_user/&&ENDUSER_password@&&database
+set serveroutput on;
+execute &&APEXRM_user..arm_installer.patch_started( -
+  i_patch_name         => 'VER-02.02.ENDUSER' -
+ ,i_patch_type         => 'feature' -
+ ,i_db_schema          => '&&ENDUSER_user' -
+ ,i_app_code           => 'LOG' -
+ ,i_branch_name        => 'feature/log/VER-02' -
+ ,i_tag_from           => 'VER-02.02A' -
+ ,i_tag_to             => 'VER-02.02B' -
+ ,i_suffix             => '' -
+ ,i_patch_desc         => 'Private synonyms for an end user' -
+ ,i_patch_components   => 'logger.syn' -
+ ,i_patch_create_date  => '02-21-2019' -
+ ,i_patch_created_by   => 'Peter' -
+ ,i_note               => '' -
+ ,i_rerunnable_yn      => 'Y' -
+ ,i_tracking_yn        => 'Y' -
+ ,i_alt_schema_yn      => 'Y' -
+ ,i_retired_yn         => 'N' -
+ ,i_remove_prereqs     => 'N' );
+
+PROMPT
+PROMPT Checking Prerequisite patch VER-02.02.LOGGER
+execute &&APEXRM_user..arm_installer.add_prereq_best_order( -
+i_patch_name     => 'VER-02.02.ENDUSER' -
+,i_prereq_patch  => 'VER-02.02.LOGGER' );
+PROMPT Ensure ARM is late enough for this patch
+execute &&APEXRM_user..arm_installer.add_prereq_best_order( -
+i_patch_name     => 'VER-02.02.ENDUSER' -
+,i_prereq_patch  => 'ARM-01.02.APEXRM' );
+select user||'@'||global_name Connection from global_name;
+
+
+PROMPT SYNONYMS
+
+PROMPT logger.syn 
+@&&patch_path.logger.syn;
+
+COMMIT;
+PROMPT Compiling objects in schema &&ENDUSER_user
+execute &&APEXRM_user..arm_invoker.compile_post_patch;
+execute &&APEXRM_user..arm_installer.patch_completed;
+COMMIT;
+PROMPT 
+PROMPT install.sql - COMPLETED.
+spool off;
+
+
+COMMIT;
+
